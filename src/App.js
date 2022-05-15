e3bee1ccfe59e6e8a8052996d12757287ae5d216
xJaimeGx <beanieman019@protonmail.com>
2022-05-15 00:02:55 -0500

    finish navbar links

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import About from './components/About';
 import Nav from './components/Nav';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
